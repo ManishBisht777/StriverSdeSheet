d37ce298b61677441d2ab3b95ac3ec2da1e70628
manish <Manishbisht9711@gmail.com>
2022-06-13 16:05:16 +0530

    delete nth ndoe form last

---
+++ LinkedList/delNthNode.cpp
@@ -0,0 +1,24 @@
+ListNode *removeNthFromEnd(ListNode *head, int n)
+{
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (n--)
+    {
+        fast = fast->next;
+    }
+
+    if (!fast)
+        return head->next;
+
+    while (fast->next)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    slow->next = slow->next->next;
+
+    return head;
+}
