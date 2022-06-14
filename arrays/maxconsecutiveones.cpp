50a1cc2f670d84d3dd0217e75909cb53ee715b80
manish <Manishbisht9711@gmail.com>
2022-06-14 18:42:57 +0530

    max consecutive ones

---
+++ arrays/maxconsecutiveones.cpp
@@ -0,0 +1,13 @@
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt=0,ans=INT_MIN;
+        
+        for(int i=0; i<nums.size();i++)
+        {
+            if(nums[i]==1) cnt++;
+            else cnt=0;
+            
+            ans=max(ans,cnt);
+        }
+        
+        return ans;
+    }
