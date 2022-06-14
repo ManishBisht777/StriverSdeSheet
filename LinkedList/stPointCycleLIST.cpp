112249b1b860b6239a626ce52285003834d55b35
manish <Manishbisht9711@gmail.com>
2022-06-14 13:11:10 +0530

    starting point in linked list

---
+++ LinkedList/stPointCycleLIST.cpp
@@ -0,0 +1,26 @@
+ListNode *detectCycle(ListNode *head)
+{
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            break;
+    }
+
+    if (!(fast && fast->next))
+        return NULL;
+
+    fast = head;
+    while (slow != fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return fast;
+}
