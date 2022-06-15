03cf220557101244b5db57c4828bdbd0ed8d0b4c
manish <Manishbisht9711@gmail.com>
2022-06-15 19:05:22 +0530

    remove duplicate from sorted list

---
+++ arrays/removeduplicate.cpp
@@ -0,0 +1,17 @@
+int removeDuplicates(vector<int> &nums)
+{
+    int ans = 1;
+    int j = 0;
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] != nums[j])
+        {
+            j++;
+            nums[j] = nums[i];
+            ans++;
+        }
+    }
+
+    return ans;
+}
