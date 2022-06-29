c2746481e8eb856744bbed30c32efccd4242cf20
manish <Manishbisht9711@gmail.com>
2022-06-29 17:07:02 +0530

    min stack

---
+++ stack Queue/minStack.cpp
@@ -0,0 +1,37 @@
+class MinStack
+{
+public:
+    stack<pair<int, int>> st;
+
+    MinStack()
+    {
+    }
+
+    void push(int val)
+    {
+
+        int mini = INT_MAX;
+
+        if (st.empty())
+            mini = val;
+        else
+            mini = min(val, st.top().second);
+
+        st.push({val, mini});
+    }
+
+    void pop()
+    {
+        st.pop();
+    }
+
+    int top()
+    {
+        return st.top().first;
+    }
+
+    int getMin()
+    {
+        return st.top().second;
+    }
+};
