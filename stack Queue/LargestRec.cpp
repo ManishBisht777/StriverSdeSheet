b306687197e50af6c723b0b8387013f8bf2c1b1d
manish <Manishbisht9711@gmail.com>
2022-06-29 14:06:25 +0530

    largest rectangle many pass soltuin

---
+++ stack Queue/LargestRec.cpp
@@ -0,0 +1,48 @@
+// many pass solution
+
+int largestRectangleArea(vector<int> &arr)
+{
+
+    stack<int> st;
+    int n = arr.size();
+    int left[n];
+    int right[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && arr[st.top()] >= arr[i])
+            st.pop();
+
+        if (st.empty())
+            left[i] = 0;
+        else
+            left[i] = st.top() + 1;
+
+        st.push(i);
+    }
+
+    while (!st.empty())
+        st.pop();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && arr[st.top()] >= arr[i])
+            st.pop();
+
+        if (st.empty())
+            right[i] = n - 1;
+        else
+            right[i] = st.top() - 1;
+
+        st.push(i);
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        ans = max(ans, arr[i] * (right[i] - left[i] + 1));
+    }
+
+    return ans;
+}
