df54eadb8e9781b7d28c8eb23fdcf6f8d696f43d
manish <Manishbisht9711@gmail.com>
2022-07-01 14:03:36 +0530

    reverse words

---
+++ string/reverseWords.cpp
@@ -0,0 +1,34 @@
+string reverseWords(string s)
+{
+
+    reverse(s.begin(), s.end());
+
+    int i = 0, l = 0, r = 0, n = s.size();
+
+    while (i < n)
+    {
+        while (i < n && s[i] != ' ')
+        {
+            s[r++] = s[i++];
+        }
+
+        if (l < r)
+        {
+            reverse(s.begin() + l, s.begin() + r);
+            if (r == n)
+                break;
+
+            s[r++] = ' ';
+            l = r;
+        }
+
+        ++i;
+    }
+
+    if (r > 0 && s[r - 1] == ' ')
+        r--;
+
+    s.resize(r);
+
+    return s;
+}
