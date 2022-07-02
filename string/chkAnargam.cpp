ee3d5f4ba5e0d0f7e034712051f4993ddd110785
manish <Manishbisht9711@gmail.com>
2022-07-02 18:43:38 +0530

    check for anargam

---
+++ string/chkAnargam.cpp
@@ -0,0 +1,17 @@
+bool isAnagram(string s, string t)
+{
+
+    if (s.size() != t.size())
+        return false;
+    int count[26] = {0};
+    for (auto ch : s)
+    {
+        count[ch - 'a']++;
+    }
+
+    for (auto ch : t)
+        if (count[ch - 'a']-- <= 0)
+            return false;
+
+    return true;
+}
