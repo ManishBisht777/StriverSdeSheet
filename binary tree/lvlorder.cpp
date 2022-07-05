2ddc54622621049a381a6f2b470b001e29463673
manish <Manishbisht9711@gmail.com>
2022-07-05 19:52:05 +0530

    level order traversal

---
+++ binary tree/lvlorder.cpp
@@ -0,0 +1,28 @@
+vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+{
+    //  Write your code here.
+
+    vector<int> ans;
+
+    if (!root)
+        return ans;
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        int n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            BinaryTreeNode<int> *node = q.front();
+            q.pop();
+            ans.push_back(node->val);
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+    }
+
+    return ans;
+}
