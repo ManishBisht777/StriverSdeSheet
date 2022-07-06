c03b45695b22ded3ca35ffbe35a0c73893cb8306
manish <Manishbisht9711@gmail.com>
2022-07-06 15:03:05 +0530

    lca

---
+++ binary tree/LCA.cpp
@@ -0,0 +1,18 @@
+
+int lowestCommonAncestor(TreeNode<int> *root, int x, int y)
+{
+    //    Write your code here
+    if (!root)
+        return -1;
+    if (root->data == x || root->data == y)
+        return root->data;
+
+    int left = lowestCommonAncestor(root->left, x, y);
+    int right = lowestCommonAncestor(root->right, x, y);
+
+    if (left == -1)
+        return right;
+    else if (right == -1)
+        return left;
+    return root->data;
+}
