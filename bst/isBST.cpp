1ac416281934de238b47e921bdbdeddad05cd6b7
manish <Manishbisht9711@gmail.com>
2022-07-11 13:08:56 +0530

    is bst

---
+++ bst/isBST.cpp
@@ -0,0 +1,16 @@
+bool solve(TreeNode *root, long min, long max)
+{
+    if (!root)
+        return true;
+
+    if (root->val >= max || root->val <= min)
+        return false;
+
+    return solve(root->left, min, root->val) && solve(root->right, root->val, max);
+}
+
+bool isValidBST(TreeNode *root)
+{
+
+    return solve(root, LONG_MIN, LONG_MAX);
+}
