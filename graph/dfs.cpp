185c967872c8320c182c7261d18738e6b5458e01
manish <Manishbisht9711@gmail.com>
2022-07-18 21:15:50 +0530

    dfs graph

---
+++ graph/dfs.cpp
@@ -0,0 +1,25 @@
+void dfs(vector<int> &ans, vector<int> &vis, vector<int> adj[], int curr)
+{
+
+    ans.push_back(curr);
+    vis[curr] = 1;
+
+    for (auto it : adj[curr])
+        if (!vis[it])
+            dfs(ans, vis, adj, it);
+}
+
+vector<int> dfsOfGraph(int v, vector<int> adj[])
+{
+    // Code here
+    vector<int> ans;
+    vector<int> vis(v, 0);
+
+    for (int i = 0; i < v; i++)
+    {
+        if (!vis[i])
+            dfs(ans, vis, adj, 0);
+    }
+
+    return ans;
+}
